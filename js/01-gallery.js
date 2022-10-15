c1b9a04de1fcf46b9b368faae4a82f7249d1437d
YanaUSA <usatiukiana@gmail.com>
2022-10-15 23:31:31 +0300

    add galleryMarkup, preventDafault to <a>, add library cdn

--- js/01-gallery.js
+++ js/01-gallery.js
@@ -1,4 +1,51 @@
 import { galleryItems } from './gallery-items.js';
 // Change code below this line
 
-console.log(galleryItems);
+const galleryRef = document.querySelector('.gallery');
+
+
+// galleryRef.addEventListener("click", );
+
+const createGalleryMarkup = galleryItems.map((item) =>
+    `<div class="gallery__item">
+    <a class="gallery__link" href="${item.original}">
+        <img
+        class="gallery__image"
+        src="${item.preview}"
+        data-source="${item.original}"
+        alt="${item.description}"
+        />
+    </a>
+    </div>`).join("");
+
+galleryRef.innerHTML = createGalleryMarkup;
+
+const galleryLinkRef = document.querySelectorAll('.gallery__link');
+
+galleryLinkRef.forEach(function (item) {
+    item.addEventListener("click", (event) => {
+        event.preventDefault();
+    });
+});
+
+import * as basicLightbox from 'basiclightbox'
+
+const instance = basicLightbox.create(`
+    <img src="assets/images/image.png" width="800" height="600">
+`)
+
+instance.show()
+
+
+
+// galleryLinkRef.addEventListener("click", (event) => {
+//     event.preventDefault();
+// });
+
+// console.log(galleryLinkSpread);
+
+
+// console.log(htmlString);
+// console.log(createGalleryMarkup);
+
+// console.log(galleryItems);
